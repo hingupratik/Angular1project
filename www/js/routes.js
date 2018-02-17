de435ab5db1f8ca45c3a9709af2d0744002ca68b
badal patel <badalpatel6534@gmail.com>
2018-02-17 23:52:29 +0530

    Resolve issue/1

---
+++ www/js/routes.js
@@ -0,0 +1,56 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider,$urlRouterProvider){
+  $stateProvider
+  .state('login',{
+    url:'/login',
+    templateUrl:'templates/login.html',
+    controller:'loginCtrl' 
+  })
+
+  $stateProvider
+  .state('register',{
+    url:'/register',
+    templateUrl:'templates/register.html', 
+    controller:'registerCtrl'
+  })
+
+  .state('main',{
+    url:'/',
+    abstract : true,
+    templateUrl:'templates/main.html',
+  })
+
+  .state('main.dash',{
+    url:'main/dash',
+    views:{
+      'dash-tab':{
+        templateUrl:'templates/dashboard.html',
+        controller:'WelcomeCtrl'
+      }
+    }
+  })
+
+  .state('main.public',{
+    url:'main/public',
+    views:{
+      'public-tab':{
+        templateUrl:'templates/public.html'
+      }
+    }
+  })
+
+  .state('main.admin',{
+    url:'main/admin',
+    views:{
+      'admin-tab':{
+        templateUrl:'templates/admin.html'
+      }
+    },
+  });
+
+
+
+  
+  $urlRouterProvider.otherwise('/main/dash')
+});
